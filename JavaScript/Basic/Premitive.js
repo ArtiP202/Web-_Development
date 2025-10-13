8bf80744eac1ebd61623f4d069d4f9c79c839fe0
Arti Pawar <artipawar2125@gmail.com>
2025-10-13 10:27:48 +0530

    Add files via upload

---
+++ JavaScript/Basic/Premitive.js
@@ -0,0 +1,10 @@
+let num=10;
+console.log(num);
+console.log(typeof(num));
+
+let Name = "Arti"
+let Arti = true;
+console.log(Name);
+console.log(typeof(Name));
+console.log(Arti);
+console.log(typeof(Arti));
