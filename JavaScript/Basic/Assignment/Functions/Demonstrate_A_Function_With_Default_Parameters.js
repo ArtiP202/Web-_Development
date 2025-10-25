58ae4d2a21e5093764611c4baf48764e25f706e7
Arti Pawar <artipawar2125@gmail.com>
2025-10-25 20:31:04 +0530

    Delete JavaScript/Basic/Assignment/Functions/Demonstrate_A_Function_With_Default_Parameters.js

--- JavaScript/Basic/Assignment/Functions/Demonstrate_A_Function_With_Default_Parameters.js
+++
@@ -1,7 +0,0 @@
-function defaultMessage(name = "Guest",Message = "Welcome To Our Javascript"){
-    console.log(`Hellow ${name}, ${Message}`);
-};
-
-defaultMessage();
-defaultMessage("Prachi");
-defaultMessage("Prachi","Have A Great Day");
