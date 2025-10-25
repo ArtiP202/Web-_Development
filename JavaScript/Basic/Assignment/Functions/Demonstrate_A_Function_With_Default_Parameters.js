2ffe28664e135262bf77d1effb6e49f2272a0b3c
Arti Pawar <artipawar2125@gmail.com>
2025-10-25 20:49:50 +0530

    Add files via upload

---
+++ JavaScript/Basic/Assignment/Functions/Demonstrate_A_Function_With_Default_Parameters.js
@@ -0,0 +1,7 @@
+function defaultMessage(name = "Guest",Message = "Welcome To Our Javascript"){
+    console.log(`Hellow ${name}, ${Message}`);
+};
+
+defaultMessage();
+defaultMessage("Arti");
+defaultMessage("Arti","Have A Great Day");
